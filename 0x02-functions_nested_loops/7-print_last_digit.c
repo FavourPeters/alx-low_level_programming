9ffa86440b2801eebb7d09ca2cf45f973641f15e
FavourPeters <favourpeters1@gmail.com>
2022-10-18 13:03:12 -0700

    commit 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit
+ * @n: the number
+ * Return: value of the last digit
+ */
+int print_last_digit(int n)
+{
+	int lastd = n % 10;
+
+	if (lastd < 0)
+		lastd *= -1;
+
+	_putchar(lastd + '0');
+
+	return (0);
+}

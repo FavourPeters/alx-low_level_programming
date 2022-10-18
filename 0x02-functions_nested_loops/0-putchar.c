22042c78661a3c87d0b0d4021d6b352bf589f896
FavourPeters <favourpeters1@gmail.com>
2022-10-18 10:41:18 -0700

    commit 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - prints _putchar, followed by a new line
+ * Return: Always 0
+ */
+int main(void)
+{
+	char alx[8] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+		_putchar(alx[i]);
+	_putchar('\n');
+
+	return (0);
+}
